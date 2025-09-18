06e1a74f08e9edf4a75d765a3fb47935e1353a29
Frady Wang <immafrady@gmail.com>
2025-09-18 16:18:57 +0800

    feat: 增加next-auth登入设置

--- app/(pages)/login/page.tsx
+++ app/(pages)/login/page.tsx
@@ -1,6 +1,5 @@
 import { GalleryVerticalEnd } from "lucide-react"
-
-import { LoginForm } from "@/components/login-form"
+import { LoginForm } from '@/components/login-form'
 
 export default function LoginPage() {
   return (
@@ -10,7 +9,7 @@ export default function LoginPage() {
           <div className="bg-primary text-primary-foreground flex size-6 items-center justify-center rounded-md">
             <GalleryVerticalEnd className="size-4" />
           </div>
-          Acme Inc.
+          Trade Journal
         </a>
         <LoginForm />
       </div>
