ef3a584d037a2a58e3b07ac771df0dfb837333c1
Frady Wang <immafrady@gmail.com>
2025-09-19 16:42:32 +0800

    feat: 增加use-mobile方法；调整global.css的位置；增加sina的get-suggestion方法；调整login的位置

---
+++ app/api/sina/get-suggestion/route.ts
@@ -0,0 +1,16 @@
+import { type NextRequest } from 'next/server'
+import iconv from 'iconv-lite'
+
+export async function GET(request: NextRequest) {
+  const searchParams = request.nextUrl.searchParams
+  const key = searchParams.get('key')
+  if (key) {
+    return fetch(`https://suggest3.sinajs.cn/suggest/type=86,87&key=${key}`).then(async (res) => {
+      const buffer = Buffer.from(await res.arrayBuffer()); // 获取原始字节流
+      // 解码成字符串
+      return new Response(iconv.decode(buffer, 'gbk'));
+    })
+  } else {
+    return new Response('No suggestions found')
+  }
+}
