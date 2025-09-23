76919c0aee61f5e34b416ac6a645872aedb2256a
Frady Wang <immafrady@gmail.com>
2025-09-23 14:38:22 +0800

    feat: 优化动画效果

--- app/(pages)/_components/app-header.tsx
+++ app/(pages)/_components/app-header.tsx
@@ -15,23 +15,23 @@ export default function AppHeader() {
   const isMe = pathname.includes('/me')
 
   return (
-    <header className="bg-muted p-2">
-      <nav className={clsx('flex items-center', isMe ? 'justify-center' : ' justify-between')}>
+    <motion.div layoutId={'header-container'} className={clsx('bg-muted p-2', isMe && 'pb-0 mb-14 rounded-b-4xl')}>
+      <nav className={clsx('flex items-center', isMe ? 'flex-col' : ' justify-between')}>
         <motion.div layoutId={'header-title'}>
           <Link className={'flex align-center'} href="/demo">
             <Logo />
             <span className={'pl-1 font-sans'}>Trade Journal</span>
           </Link>
         </motion.div>
-        {!isMe && (<motion.div layoutId={'user-avatar'} className={'z-50'}>
+        <motion.div layoutId={'user-avatar'} className={clsx(isMe && 'translate-y-1/2')}>
           <Link href="/me">
-          <Avatar>
-            <AvatarImage src={userMeta?.avatar}/>
-            <AvatarFallback>🤓</AvatarFallback>
-          </Avatar>
+            <Avatar className={clsx(isMe && 'size-28')}>
+              <AvatarImage src={userMeta?.avatar}/>
+              <AvatarFallback>🤓</AvatarFallback>
+            </Avatar>
           </Link>
-        </motion.div>)}
+        </motion.div>
       </nav>
-    </header>
+    </motion.div>
   );
 }
--- app/(pages)/me/page.tsx
+++ app/(pages)/me/page.tsx
@@ -1,27 +1,16 @@
 'use client'
 
 import React from 'react'
-import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
 import { UserMetaContext } from '@/providers/user-meta'
 import ListItem from '@/app/(pages)/me/_components/list-item'
 import { MailIcon, User2Icon} from 'lucide-react'
-import { motion } from 'motion/react'
 
 export default function Page() {
   const userMeta = React.useContext(UserMetaContext)
 
   return (
     <div className={'flex flex-col items-center'}>
-      <div className={'flex flex-col items-center p-5'}>
-        <div className={'fixed bg-muted top-0  w-full h-40 -z-50 rounded-b-4xl'}></div>
-        <motion.div layoutId={'user-avatar'} className={'z-50'}>
-          <Avatar className={'size-28'}>
-            <AvatarImage src={userMeta?.avatar}/>
-            <AvatarFallback>{userMeta?.name}</AvatarFallback>
-          </Avatar>
-        </motion.div>
-        <span className={'font-bold font-serif'}>{userMeta?.name}</span>
-      </div>
+      <div className={'font-bold font-serif'}>{userMeta?.name}</div>
       <ul className={'self-stretch'}>
         <ListItem icon={<User2Icon />} label={'用户名'} value={userMeta?.username ?? ''} />
         <ListItem icon={<MailIcon />} label={'E-Mail'} value={userMeta?.email ?? ''} />
