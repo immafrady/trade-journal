48fd8567e99188464a7943fb758a6056a1cd8927
Frady Wang <immafrady@gmail.com>
2025-09-23 13:39:21 +0800

    feat: 增加跳页动效

--- app/(pages)/_components/app-header.tsx
+++ app/(pages)/_components/app-header.tsx
@@ -10,23 +10,27 @@ import { motion } from "motion/react"
 
 export default function AppHeader() {
   const userMeta = React.useContext(UserMetaContext)
+  console.log(userMeta)
   const pathname = usePathname()
   const isMe = pathname.includes('/me')
 
   return (
     <header className="bg-muted p-2">
       <nav className={clsx('flex items-center', isMe ? 'justify-center' : ' justify-between')}>
-        <Link className={'flex align-center'} href="/demo">
-          <Logo />
-          <span className={'pl-1 font-sans'}>Trade Journal</span>
-        </Link>
-        {!isMe && (<Link href="/me">
+        <motion.div layoutId={'header-title'}>
+          <Link className={'flex align-center'} href="/demo">
+            <Logo />
+            <span className={'pl-1 font-sans'}>Trade Journal</span>
+          </Link>
+        </motion.div>
+        {!isMe && (<motion.div layoutId={'user-avatar'} className={'z-50'}>
+          <Link href="/me">
           <Avatar>
             <AvatarImage src={userMeta?.avatar}/>
             <AvatarFallback>🤓</AvatarFallback>
           </Avatar>
-        </Link>)}
-
+          </Link>
+        </motion.div>)}
       </nav>
     </header>
   );
--- app/(pages)/me/page.tsx
+++ app/(pages)/me/page.tsx
@@ -5,6 +5,7 @@ import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
 import { UserMetaContext } from '@/providers/user-meta'
 import ListItem from '@/app/(pages)/me/_components/list-item'
 import { MailIcon, User2Icon} from 'lucide-react'
+import { motion } from 'motion/react'
 
 export default function Page() {
   const userMeta = React.useContext(UserMetaContext)
@@ -13,10 +14,12 @@ export default function Page() {
     <div className={'flex flex-col items-center'}>
       <div className={'flex flex-col items-center p-5'}>
         <div className={'fixed bg-muted top-0  w-full h-40 -z-50 rounded-b-4xl'}></div>
-        <Avatar className={'size-28'}>
-          <AvatarImage src={userMeta?.avatar}/>
-          <AvatarFallback>{userMeta?.name}</AvatarFallback>
-        </Avatar>
+        <motion.div layoutId={'user-avatar'} className={'z-50'}>
+          <Avatar className={'size-28'}>
+            <AvatarImage src={userMeta?.avatar}/>
+            <AvatarFallback>{userMeta?.name}</AvatarFallback>
+          </Avatar>
+        </motion.div>
         <span className={'font-bold font-serif'}>{userMeta?.name}</span>
       </div>
       <ul className={'self-stretch'}>
