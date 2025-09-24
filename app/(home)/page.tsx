bb8e744bac1b8ef53b8943b98e9a0027f738cf40
fradywang <immafrady@gmail.com>
2025-09-24 15:35:32 +0800

    feat: 使用swr来解决请求值缓存的问题

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -2,33 +2,25 @@
 import React from 'react'
 import { createClient } from '@/lib/supabase/browser-client'
 import { Button } from '@/components/ui/button'
-import {getList, UserHolding} from '@/lib/services/user-holdings'
+import { useHoldingList } from '@/lib/services/user-holdings'
 import Loading from './loading'
 
 export default function Page() {
-  const [list, setList] = React.useState<UserHolding[]|null>(null);
-  React.useEffect(() => {
-    getList().then((list) => {
-      console.log(list)
-      setList(list)
-    })
-    return () => {}
-  }, [])
+  const { data, isLoading, mutate } = useHoldingList()
   return (
     <>
       <div>
-        <pre>{JSON.stringify(list, null, 2)}</pre>
+        <pre>{JSON.stringify(data, null, 2)}</pre>
         <div>add one</div>
         <Button onClick={async () => {
           const supabase = createClient()
           await supabase.from('user_holdings').insert([{
             code: '1', type: '1'
           }]).select()
-          getList().then((list) => setList(list))
-
+          await mutate()
         }}>+1</Button>
       </div>
-      <Loading isLoading={list === null} />
+      <Loading isLoading={isLoading} />
     </>
   )
 }
--- lib/services/user-holdings.ts
+++ lib/services/user-holdings.ts
@@ -1,3 +1,5 @@
+import useSWR, {SWRResponse} from 'swr'
+
 export interface UserHolding {
   id: number,
   userId: string,
@@ -6,12 +8,10 @@ export interface UserHolding {
   type: string
 }
 /// 查列表
-export const getList: () => Promise<UserHolding[]> = async () => {
-  return fetch('/api/actions/holdings').then(async response => {
-    const { data, error } = await response.json()
-    if (error) throw error
-
-    console.log('in client?')
+export function useHoldingList(): SWRResponse<UserHolding[], any, any> {
+  return useSWR('/api/actions/holdings', async (key) => {
+    const response = await fetch(key)
+    const {data} = await response.json()
     return (data || []).map((holding: any) => ({
       id: holding.id,
       userId: holding.user_id,
