4d6599c9345e920f032e7963b17ff7f072cff1a5
fradywang <immafrady@gmail.com>
2025-09-24 15:49:27 +0800

    feat: 调整页面布局

--- app/(home)/layout.tsx
+++ app/(home)/layout.tsx
@@ -5,7 +5,7 @@ import { UserMetaProvider } from '@/providers/user-meta'
 export default function BaseLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
     <UserMetaProvider>
-      <div className={'flex flex-col overflow-hidden max-h-screen'}>
+      <div className={'flex flex-col overflow-hidden h-svh'}>
         <AppHeader />
         <main className={'relative flex-1 p-4 overflow-x-hidden overflow-y-auto'}>
           {children}
