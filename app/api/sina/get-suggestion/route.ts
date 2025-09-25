955b614d06262b4dde854cbc555c09f24c8a33df
Frady Wang <immafrady@gmail.com>
2025-09-25 18:22:35 +0800

    feat: 调整新浪的查询类型

--- app/api/sina/get-suggestion/route.ts
+++ app/api/sina/get-suggestion/route.ts
@@ -1,12 +1,13 @@
 import { NextResponse, type NextRequest } from "next/server";
 import iconv from "iconv-lite";
+import { SINA_SEARCH_VALUES } from "@/lib/enums/sina-stock-type";
 
 export async function GET(request: NextRequest) {
   const searchParams = request.nextUrl.searchParams;
   const key = searchParams.get("key");
   if (key) {
     return fetch(
-      `https://suggest3.sinajs.cn/suggest/type=86,87&key=${key}`,
+      `https://suggest3.sinajs.cn/suggest/type=${SINA_SEARCH_VALUES}&key=${key}`,
     ).then(async (res) => {
       const buffer = Buffer.from(await res.arrayBuffer()); // 获取原始字节流
       // 解码成字符串
