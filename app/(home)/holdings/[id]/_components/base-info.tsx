b8d7f6b30d077bc21a3a3941ddfcb02f0a457ecd
fradywang <immafrady@gmail.com>
2025-09-30 22:46:32 +0800

    feat: 将AlertDialog抽公共

--- app/(home)/holdings/[id]/_components/base-info.tsx
+++ app/(home)/holdings/[id]/_components/base-info.tsx
@@ -9,23 +9,15 @@ import {
 import { Button } from "@/components/ui/button";
 import { Trash } from "lucide-react";
 import React from "react";
-import {
-  AlertDialog,
-  AlertDialogAction,
-  AlertDialogCancel,
-  AlertDialogContent,
-  AlertDialogDescription,
-  AlertDialogFooter,
-  AlertDialogHeader,
-  AlertDialogTitle,
-  AlertDialogTrigger,
-} from "@/components/ui/alert-dialog";
 import { deleteHolding } from "@/lib/services/holdings/holding-apis";
 import { toast } from "sonner";
 import { useRouter } from "next/navigation";
+import { MyAlertDialog } from "@/components/ui/my/alert-dialog";
+import { useHoldingList } from "@/lib/services/holdings/use-holding-list";
 
 export const BaseInfo = ({ data }: { data: HoldingWithQuote }) => {
   const route = useRouter();
+  const { mutate } = useHoldingList();
   return (
     <Card>
       <CardHeader>
@@ -36,38 +28,27 @@ export const BaseInfo = ({ data }: { data: HoldingWithQuote }) => {
             </Badge>
             {data.ticker.label}
           </div>
-          <AlertDialog>
-            <AlertDialogTrigger asChild>
+          <MyAlertDialog
+            trigger={
               <Button variant={"destructive"} className={"rounded-full"}>
                 <Trash />
               </Button>
-            </AlertDialogTrigger>
-            <AlertDialogContent>
-              <AlertDialogHeader>
-                <AlertDialogTitle>确定删除?</AlertDialogTitle>
-                <AlertDialogDescription>
-                  一旦删除，该标的关联的持仓信息将会一并删除
-                </AlertDialogDescription>
-              </AlertDialogHeader>
-              <AlertDialogFooter>
-                <AlertDialogCancel>取消</AlertDialogCancel>
-                <AlertDialogAction
-                  onClick={async () => {
-                    const response = await deleteHolding(data.id);
-                    const { message, error } = await response.json();
-                    if (error) {
-                      toast.error(error, { position: "top-center" });
-                    } else {
-                      toast.success(message, { position: "top-center" });
-                      route.replace("/");
-                    }
-                  }}
-                >
-                  确认
-                </AlertDialogAction>
-              </AlertDialogFooter>
-            </AlertDialogContent>
-          </AlertDialog>
+            }
+            title={"确定删除?"}
+            description={"一旦删除，该标的关联的持仓信息将会一并删除"}
+            showCancel={true}
+            onConfirm={async () => {
+              const response = await deleteHolding(data.id);
+              const { message, error } = await response.json();
+              if (error) {
+                toast.error(error, { position: "top-center" });
+              } else {
+                await mutate();
+                toast.success(message, { position: "top-center" });
+                route.replace("/");
+              }
+            }}
+          />
         </CardTitle>
       </CardHeader>
     </Card>
