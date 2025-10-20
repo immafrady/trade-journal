d49e14f74f8e9fb26840af5aca8488eb040c6704
fradywang <immafrady@gmail.com>
2025-10-21 00:49:41 +0800

    perf: 按钮组移位，优化样式

--- app/(home)/holdings/[id]/_components/data-page/index.tsx
+++ app/(home)/holdings/[id]/_components/data-page/index.tsx
@@ -1,6 +1,10 @@
 import { useHoldingInfo } from "@/app/(home)/holdings/[id]/_hooks/use-holding-info";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { TabSummary } from "@/app/(home)/holdings/[id]/_components/data-page/tab-summary";
+import { ButtonGroup } from "@/components/ui/button-group";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export const DataPage = ({
   onTabChange,
@@ -8,25 +12,52 @@ export const DataPage = ({
   onTabChange: (isSummary: boolean) => void;
 }) => {
   const { id, data } = useHoldingInfo();
+  const pathname = usePathname(); // 例如 /holdings/10
 
   return (
-    <Tabs
-      defaultValue={TabKey.Summary}
-      className="w-full"
-      onValueChange={(t) => onTabChange(t === TabKey.Summary)}
-    >
-      <div className={"flex justify-center"}>
-        <TabsList>
-          <TabsTrigger value={TabKey.Summary}>基本信息</TabsTrigger>
-          <TabsTrigger value={TabKey.Table}>Password</TabsTrigger>
-        </TabsList>
-      </div>
+    <div className={"relative pb-safe-offset-20"}>
+      <Tabs
+        defaultValue={TabKey.Summary}
+        className="w-full"
+        onValueChange={(t) => onTabChange(t === TabKey.Summary)}
+      >
+        <div className={"flex justify-center"}>
+          <TabsList>
+            <TabsTrigger value={TabKey.Summary}>基本信息</TabsTrigger>
+            <TabsTrigger value={TabKey.Table}>Password</TabsTrigger>
+          </TabsList>
+        </div>
 
-      <TabsContent value={TabKey.Summary}>
-        <TabSummary />
-      </TabsContent>
-      <TabsContent value={TabKey.Table}>Change your password here.</TabsContent>
-    </Tabs>
+        <TabsContent value={TabKey.Summary}>
+          <TabSummary />
+        </TabsContent>
+        <TabsContent value={TabKey.Table}>
+          Change your password here.
+        </TabsContent>
+      </Tabs>
+      <div
+        className={
+          "fixed bottom-0 left-0 right-0 p-safe-offset-2 flex justify-center gap-4 bg-muted"
+        }
+      >
+        <ButtonGroup>
+          <ButtonGroup>
+            <Button asChild>
+              <Link href={`${pathname}/add`}>新增一条</Link>
+            </Button>
+            <Button variant={"outline"} asChild>
+              <Link href={`${pathname}/import`}>导入CSV</Link>
+            </Button>
+          </ButtonGroup>
+        </ButtonGroup>
+        <Button variant={"outline"} asChild>
+          <Link href={`${pathname}/import`}>导出CSV</Link>
+        </Button>
+        <Button variant={"destructive"} asChild>
+          <Link href={`${pathname}/import`}>清除数据</Link>
+        </Button>
+      </div>
+    </div>
   );
 };
 
--- app/(home)/holdings/[id]/_components/data-page/tab-summary.tsx
+++ app/(home)/holdings/[id]/_components/data-page/tab-summary.tsx
@@ -1,36 +1,3 @@
-import { Button } from "@/components/ui/button";
-import Link from "next/link";
-import { usePathname } from "next/navigation";
-import { ButtonGroup } from "@/components/ui/button-group";
-
 export const TabSummary = () => {
-  const pathname = usePathname(); // 例如 /holdings/10
-
-  return (
-    <div className={"relative"}>
-      dkflsjdlfk
-      <div
-        className={
-          "fixed bottom-0 left-0 right-0 p-safe-offset-2 flex justify-center gap-4 bg-muted"
-        }
-      >
-        <ButtonGroup>
-          <ButtonGroup>
-            <Button asChild>
-              <Link href={`${pathname}/add`}>新增一条</Link>
-            </Button>
-            <Button variant={"outline"} asChild>
-              <Link href={`${pathname}/import`}>导入CSV</Link>
-            </Button>
-          </ButtonGroup>
-        </ButtonGroup>
-        <Button variant={"outline"} asChild>
-          <Link href={`${pathname}/import`}>导出CSV</Link>
-        </Button>
-        <Button variant={"destructive"} asChild>
-          <Link href={`${pathname}/import`}>清除数据</Link>
-        </Button>
-      </div>
-    </div>
-  );
+  return <div className={"relative"}></div>;
 };
