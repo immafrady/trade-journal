97c012e0c83dd3b44067e1111403f8817ae2264a
fradywang <immafrady@gmail.com>
2025-10-22 01:48:44 +0800

    temp: 定义部分useTradeRecordSummary代码

--- lib/services/trade-records/use-trade-record-summary.ts
+++ lib/services/trade-records/use-trade-record-summary.ts
@@ -1,6 +1,17 @@
-import { useTradeRecordList } from "@/lib/services/trade-records/use-trade-record-list";
+import { useTradeRecordExtendList } from "@/lib/services/trade-records/use-trade-record-extend-list";
 
 export const useTradeRecordSummary = (holdingId: string) => {
-  const list = useTradeRecordList(holdingId);
+  const list = useTradeRecordExtendList(holdingId);
   // 汇总操作次数、金额合计
+  let totalFee = 0;
+  for (const item of list) {
+    totalFee += item.record.adjusted.fee;
+  }
+  return {
+    totalFee,
+  } as TradeRecordSummary;
 };
+
+export interface TradeRecordSummary {
+  totalFee: number;
+}
