3f9bf90c769d5a9cb9a7932e9ff006671b568313
Frady Wang <immafrady@gmail.com>
2025-10-27 14:50:02 +0800

    chore: vercel/supabase适应化开发

--- app/login/_components/login-form.tsx
+++ app/login/_components/login-form.tsx
@@ -32,14 +32,21 @@ export function LoginForm({
                   className="w-full"
                   onClick={async (e) => {
                     e.preventDefault();
+                    let url =
+                      process?.env?.NEXT_PUBLIC_SITE_URL ?? // Set this to your site URL in production env.
+                      process?.env?.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel.
+                      `${location.origin}/`;
+                    // Make sure to include `https://` when not localhost.
+                    url = url.startsWith("http") ? url : `https://${url}`;
+                    // Make sure to include a trailing `/`.
+                    url = url.endsWith("/") ? url : `${url}/`;
+
                     const supabase = createClient();
+
                     await supabase.auth.signInWithOAuth({
                       provider: "github",
                       options: {
-                        redirectTo: new URL(
-                          "/api/auth/callback?next",
-                          location.origin,
-                        ).toString(),
+                        redirectTo: url,
                       },
                     });
                   }}
