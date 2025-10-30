6bde9b981629e07f80a7e8f5e3056508a4bc4ffd
Frady Wang <immafrady@gmail.com>
2025-10-30 18:32:28 +0800

    fix: 修复中间件拦截导致的manifest.webmanifest文件访问不了

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,8 @@
-import { type NextRequest } from 'next/server'
-import { updateSession } from '@/lib/supabase/middleware'
+import { type NextRequest } from "next/server";
+import { updateSession } from "@/lib/supabase/middleware";
 
 export async function middleware(request: NextRequest) {
-  return await updateSession(request)
+  return await updateSession(request);
 }
 export const config = {
   matcher: [
@@ -13,6 +13,6 @@ export const config = {
      * - favicon.ico (favicon file)
      * Feel free to modify this pattern to include more paths.
      */
-    '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
+    "/((?!_next/static|_next/image|favicon.ico|manifest.webmanifest|.*\\.(?:svg|png|jpg|jpeg|gif|webp|)$).*)",
   ],
-}
+};
