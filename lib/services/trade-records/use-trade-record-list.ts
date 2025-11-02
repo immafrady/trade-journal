12ed697a43858df2cb57b250c862fe54420a5128
fradywang <immafrady@gmail.com>
2025-11-02 17:07:16 +0800

    feat: 增加累计计算包含复权

--- lib/services/trade-records/use-trade-record-list.ts
+++ lib/services/trade-records/use-trade-record-list.ts
@@ -1,6 +1,7 @@
 import useSWR, { SWRResponse } from "swr";
 import { TradeRecord } from "@/lib/services/trade-records/trade-record";
 import React from "react";
+import { TradeRecordType } from "@/lib/enums/trade-record-type";
 
 export function useTradeRecordList(
   holdingId: string,
@@ -25,16 +26,41 @@ export function useTradeRecordList(
       let totalShares = 0;
       let totalAmount = 0;
       const result: TradeRecord[] = [];
+
       for (let i = list.length - 1; i >= 0; i--) {
         const record = TradeRecord.fromDatabase(list[i]);
-        totalShares += record.adjusted.shares;
         totalAmount += record.adjusted.amount;
+        if (
+          [TradeRecordType.Split, TradeRecordType.Merge].includes(
+            record.props.type,
+          )
+        ) {
+          const afterShares = totalShares + record.adjusted.shares;
+          const ratio = afterShares / totalShares; // 计算出变化幅度
+          for (const r of result) {
+            // 重新将之前的再计算一遍
+            r.cumulative.totalShares *= ratio;
+            r.cumulative.costPrice =
+              r.cumulative.totalShares > 0
+                ? r.cumulative.totalAmount / r.cumulative.totalShares
+                : 0;
+          }
+          totalShares = afterShares;
+        } else {
+          totalShares += record.adjusted.shares;
+        }
         record.cumulative = {
           totalAmount,
           totalShares,
           costPrice: totalShares > 0 ? totalAmount / totalShares : 0,
         };
         result.unshift(record);
+        if (
+          [TradeRecordType.Split, TradeRecordType.Merge].includes(
+            record.props.type,
+          )
+        ) {
+        }
       }
       return result;
     } catch (e) {
