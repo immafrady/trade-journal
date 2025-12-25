054acf8e675bf533fcfabdfef1bdb56534cc14d1
Frady Wang <immafrady@gmail.com>
2025-12-25 12:04:01 +0800

    fix：补充回来next.config.ts，并增加serwist仅在生产开启的判断

---
+++ next.config.ts
@@ -0,0 +1,56 @@
+import withSerwistInit from "@serwist/next";
+
+const withSerwist = withSerwistInit({
+  swSrc: "app/sw.ts",
+  swDest: "public/sw.js",
+  // 👇 关键配置：当环境为 development 时禁用 Serwist
+  disable: process.env.NODE_ENV === "development",
+});
+
+export default withSerwist({
+  /* config options here */
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "X-Content-Type-Options",
+            value: "nosniff",
+          },
+          {
+            key: "X-Frame-Options",
+            value: "DENY",
+          },
+          {
+            key: "Referrer-Policy",
+            value: "strict-origin-when-cross-origin",
+          },
+        ],
+      },
+      {
+        source: "/sw.js",
+        headers: [
+          {
+            key: "Content-Type",
+            value: "application/javascript; charset=utf-8",
+          },
+          {
+            key: "Cache-Control",
+            value: "no-cache, no-store, must-revalidate",
+          },
+          {
+            key: "Content-Security-Policy",
+            value:
+              "default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline'",
+          },
+        ],
+      },
+    ];
+  },
+});
