fdbbb29f0a991b2f8a86959b25f9ce73f354e1d4
Frady Wang <immafrady@gmail.com>
2026-01-23 11:07:34 +0800

    fix: AppBarAlert增加layoutId

--- components/layout/app-shell/app-bar-element/alert.tsx
+++ components/layout/app-shell/app-bar-element/alert.tsx
@@ -3,19 +3,22 @@ import React from "react";
 import { TriangleAlert } from "lucide-react";
 import { Avatar, AvatarFallback } from "@/components/ui/avatar";
 import { useTradeRecordStore } from "@/lib/services/trade-records";
+import { motion } from "motion/react";
 
 export const AppBarAlert = () => {
   const draftList = useTradeRecordStore((s) => s.draftList) ?? [];
 
   if (draftList.length) {
     return (
-      <Link href="/alert-messages">
-        <Avatar>
-          <AvatarFallback className={"bg-orange-300/20 animate-pulse"}>
-            <TriangleAlert className="h-5 w-5 text-orange-600" />
-          </AvatarFallback>
-        </Avatar>
-      </Link>
+      <motion.div layoutId={"app-bar-alert"}>
+        <Link href="/alert-messages">
+          <Avatar>
+            <AvatarFallback className={"bg-orange-300/20 animate-pulse"}>
+              <TriangleAlert className="h-5 w-5 text-orange-600" />
+            </AvatarFallback>
+          </Avatar>
+        </Link>
+      </motion.div>
     );
   }
   return null;
