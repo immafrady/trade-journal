549160caf0d02026a7f1c24e9d06ebfd911405f3
Frady Wang <immafrady@gmail.com>
2026-01-28 11:28:30 +0800

    feat: Group - 完成新增弹窗逻辑

--- lib/services/group/group-apis.ts
+++ lib/services/group/group-apis.ts
@@ -1,8 +1,8 @@
 import { GroupModel } from "@/lib/services/group/domain/group";
 
 // 新增分组
-export async function addGroup(group: GroupModel) {
-  return await fetch("/api/groups", {
+export async function addOrEditGroup(group: GroupModel) {
+  return await fetch("/api/actions/groups", {
     method: "POST",
     headers: { "content-type": "application/json" },
     body: JSON.stringify({
@@ -14,17 +14,17 @@ export async function addGroup(group: GroupModel) {
 }
 
 // 编辑分组持仓
-export async function editGroupHoldings(group: GroupModel) {
-  return await fetch(`/api/groups/${group.id}/holdings`, {
+export async function editGroupHoldings(groupId: string, holdingIds: string[]) {
+  return await fetch(`/api/actions/groups/${groupId}/holdings`, {
     method: "POST",
     headers: { "content-type": "application/json" },
-    body: JSON.stringify(group.holdingIds),
+    body: JSON.stringify(holdingIds),
   });
 }
 
 // 删除分组
 export async function deleteGroup(id: string) {
-  return await fetch(`/api/groups/${id}`, {
+  return await fetch(`/api/actions/groups/${id}`, {
     method: "DELETE",
   });
 }
