d87f78b5bf1cab19410b732e948b6c309d97a558
Frady Wang <immafrady@gmail.com>
2026-01-30 12:05:06 +0800

    feat: Group - useGroupTradeRecord调整算法

--- lib/services/group/hooks/use-group-trade-records.ts
+++ lib/services/group/hooks/use-group-trade-records.ts
@@ -7,7 +7,7 @@ export const useGroupTradeRecords = (holdingIds: string[]) => {
 
   return React.useMemo(() => {
     const result: TradeRecordExtend[] = [];
-    const holdingState = new Map<string, ReturnType<typeof getStateInfo>>();
+    const marketValueMap = new Map<string, number>();
 
     // 👇 1️⃣ 先把所有记录摊平成时间线
     const timeline: TradeRecordExtend[] = [];
@@ -18,31 +18,23 @@ export const useGroupTradeRecords = (holdingIds: string[]) => {
       }
     }
 
-    // 👇 2️⃣ 按时间从旧到新排序
-    timeline.sort(
-      (a, b) =>
-        a.record.props.tradedAt.valueOf() - b.record.props.tradedAt.valueOf(),
-    );
+    // 👇 2️⃣ 按时间从旧到新排序，其次按照id从旧到新排序
+    timeline.sort((a, b) => {
+      const timeDiff =
+        a.record.props.tradedAt.valueOf() - b.record.props.tradedAt.valueOf();
+      if (!timeDiff) return timeDiff;
+      return a.record.props.id! - b.record.props.id!;
+    });
 
     let totalAmount = 0;
-    let marketValue = 0;
     // 👇 3️⃣ 沿时间轴推进（核心算法 O(n)）
     for (const tre of timeline) {
-      const prev = holdingState.get(tre.holdingId) ?? getStateInfo();
-      // 先扣旧值
-      totalAmount -= prev.totalAmount;
-      marketValue -= prev.marketValue;
-      // 存新状态
-      holdingState.set(
-        tre.holdingId,
-        getStateInfo(
-          tre.record.cumulative.totalAmount,
-          tre.record.cumulative.marketValue,
-        ),
-      );
-      // 加回组合
-      totalAmount += tre.record.cumulative.totalAmount;
-      marketValue += tre.record.cumulative.marketValue;
+      totalAmount += tre.record.adjusted.amount;
+      marketValueMap.set(tre.holdingId, tre.record.cumulative.marketValue);
+      const marketValue = marketValueMap
+        .values()
+        .reduce((prev, curr) => prev + curr, 0);
+
       const valueIndex = totalAmount === 0 ? 0 : marketValue / totalAmount;
 
       tre.group = {
@@ -55,10 +47,3 @@ export const useGroupTradeRecords = (holdingIds: string[]) => {
     return result;
   }, [tickerMap, holdingRecordMap]);
 };
-
-function getStateInfo(totalAmount = 0, marketValue = 0) {
-  return {
-    totalAmount,
-    marketValue,
-  };
-}
