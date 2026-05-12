2208b127c75d1f1999f0b7c9ae7ea53072b79c92
fradywang <immafrady@gmail.com>
2026-05-12 19:11:53 +0800

    feat: 增加删除组合功能

--- app/(home)/groups/[id]/_components/tab-summary/bottom-bar.tsx
+++ app/(home)/groups/[id]/_components/tab-summary/bottom-bar.tsx
@@ -1,12 +1,19 @@
 import { BottomBarContainer } from "@/components/ui/my/bottom-bar-container";
 import { DialogEdit } from "@/app/(home)/groups/_components/dialog-edit";
 import { Button } from "@/components/ui/button";
-import { Pencil } from "lucide-react";
+import { Pencil, Trash } from "lucide-react";
 import React from "react";
 import { GroupInfoContext } from "@/app/(home)/groups/[id]/_providers";
+import { MyAlertDialog } from "@/components/ui/my/alert-dialog";
+import { deleteGroup, useGroupList } from "@/lib/services/group";
+import { toast } from "sonner";
+import { useRouter } from "next/navigation";
 
 export const BottomBar = () => {
   const model = React.useContext(GroupInfoContext)!;
+  const { mutate } = useGroupList();
+  const route = useRouter();
+
   return (
     <BottomBarContainer>
       <DialogEdit
@@ -18,6 +25,28 @@ export const BottomBar = () => {
           </Button>
         }
       ></DialogEdit>
+      <MyAlertDialog
+        trigger={
+          <Button variant={"destructive"} size="sm">
+            <Trash />
+            删除组合
+          </Button>
+        }
+        title={"确定删除?"}
+        description={"删除组合不影响关联标的"}
+        showCancel={true}
+        onConfirm={async () => {
+          const response = await deleteGroup(model.id!);
+          const { message, error } = await response.json();
+          if (error) {
+            toast.error(error);
+          } else {
+            route.replace("/groups");
+            await mutate();
+            toast.success(message);
+          }
+        }}
+      />
     </BottomBarContainer>
   );
 };
